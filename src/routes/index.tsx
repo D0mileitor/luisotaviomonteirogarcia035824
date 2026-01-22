4e3f7d742c795a2efa22084e7c71ac1fe3022a3a
Domi.PC <luisotaviomonteirogarcia@gmail.com>
2026-01-21 22:38:29 -0400

    feat: ajustando redirect inicial do / para /login

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 import Login from "@/pages/Login"
 import PetsLista from "@/pages/PetsLista"
 
@@ -6,8 +6,10 @@ export function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<Login />} />
         <Route path="/pets" element={<PetsLista />} />
+        <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
     </BrowserRouter>
   )
